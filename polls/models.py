518890b00af2ce9b1a96947a41a0586bb087d5f3
klemen <klemen.kolesa@gmail.com>
2024-11-07 18:30:21 +0100

    added update and create methods on polls api

--- polls/models.py
+++ polls/models.py
@@ -28,7 +28,7 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='choices')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='choices', blank=True)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
--- polls/serializers.py
+++ polls/serializers.py
@@ -18,3 +18,41 @@ class QuestionSerializer(serializers.HyperlinkedModelSerializer):
         extra_kwargs = {
             'url': {'view_name': 'polls:question-detail', 'lookup_field': 'id'}  # Ensure correct view name and lookup
         }
+
+    def update(self, instance, validated_data):
+        choices_data = validated_data.pop('choices')
+
+        instance = super().update(instance, validated_data)
+
+        instance.choices.all().delete()
+
+        # update or create choices, remove choices that were not included in the update request
+        choice_ids = []
+
+        for choice_data in choices_data:
+            choice_id = choice_data.get('id')
+            choice_text = choice_data.get('choice_text')
+
+            if choice_id:
+                try:
+                    choice = Choice.objects.get(pk=choice_id)
+                    choice.choice_text = choice_text
+                    choice.save()
+                    choice_ids.append(choice_id)
+                except Choice.DoesNotExist:
+                    raise serializers.ValidationError(f"Choice with id {choice_id} does not exist.")
+            else:
+                choice = Choice.objects.create(question=instance, choice_text=choice_text)
+                choice_ids.append(choice.id)
+
+        # Remove choices that were not included in the update request
+        instance.choices.exclude(id__in=choice_ids).delete()
+
+        return instance
+
+    def create(self, validated_data):
+        choices_data = validated_data.pop('choices')
+        question = Question.objects.create(**validated_data)
+        for choice_data in choices_data:
+            Choice.objects.create(question=question, choice_text=choice_data.get('choice_text'))
+        return question
